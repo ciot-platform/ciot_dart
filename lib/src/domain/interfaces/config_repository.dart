ad9ac7e1ae08d42cc33d5223e21064ae15e2d792
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-07-24 09:30:24 -0300

    fix: update import path in config_repository.dart for correct module resolution

--- lib/src/domain/interfaces/config_repository.dart
+++ lib/src/domain/interfaces/config_repository.dart
@@ -1,5 +1,5 @@
 import 'package:ciot_dart/src/errors/errors.dart';
-import 'package:ciot_dart/src/generated/ciot/proto/v2/msg.pb.dart';
+import 'package:ciot_dart/generated/ciot/proto/v2/msg.pb.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract class ConfigRepository {
