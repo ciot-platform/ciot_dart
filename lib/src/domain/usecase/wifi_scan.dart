521b23c46e40d568808ed34fd9bdbc800b9de01b
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-18 08:52:35 -0300

    feat: implement WifiScan and WifiScanImpl classes for WiFi scanning functionality

---
+++ lib/src/domain/usecase/wifi_scan.dart
@@ -0,0 +1,7 @@
+import 'package:ciot_dart/ciot.dart';
+import 'package:ciot_dart/generated/ciot/proto/v2/wifi.pb.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract class WifiScan {
+  Future<Either<ErrorBase, List<WifiApInfo>>> scan();
+}
