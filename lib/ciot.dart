f27478a25e7b268836f427451b96008adedc44e2
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-11-21 10:38:31 -0300

    feat: add serializer and http_server exports to ciot.dart

--- lib/ciot.dart
+++ lib/ciot.dart
@@ -2,5 +2,7 @@ library;
 
 export 'src/domain/interfaces/iface.dart';
 export 'src/domain/interfaces/iface_base.dart';
+export 'src/domain/interfaces/serializer.dart';
 export 'src/infra/interfaces/http_client.dart';
+export 'src/infra/interfaces/http_server.dart';
 export 'src/errors/errors.dart';
