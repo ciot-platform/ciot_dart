38152104810a4838bb0dc2e22405fdf6bfacfdad
wesley-santos <53940899+wjsan@users.noreply.github.com>
2026-02-04 12:41:50 -0300

    feat: add new error classes for enhanced error handling

--- lib/src/errors/errors.dart
+++ lib/src/errors/errors.dart
@@ -22,7 +22,11 @@ class ErrorNotFound implements ErrorBase {}
 
 class ErrorTimeout implements ErrorBase {}
 
+class ErrorConnect extends ErrorBase {}
+
 class ErrorConnection implements ErrorBase {}
+
+class ErrorDisconnection implements ErrorBase {}
 
 class ErrorNullConfig implements ErrorBase {}
 
@@ -30,7 +34,34 @@ class ErrorInvalidState implements ErrorBase {}
 
 class ErrorBusy implements ErrorBase {}
 
+class ErrorValidation extends ErrorBase {}
+
+class ErrorCredentials extends ErrorBase {}
+
+class ErrorAlreadyExists extends ErrorBase {}
+
+class ErrorInvalidData extends ErrorBase {}
+
+class ErrorNotSupported extends ErrorBase {}
+
+class ErrorNetwork extends ErrorBase {}
+
+class ErrorServer extends ErrorBase {}
+
+class ErrorUnauthorized extends ErrorBase {}
+
+class ErrorInvalidRole extends ErrorBase {}
+
+class ErrorInvalidStatusCode extends ErrorBase {
+  int statusCode;
+  ErrorInvalidStatusCode(this.statusCode);
+}
+
 class ErrorCiotRequest implements ErrorBase {
   final Err error;
   ErrorCiotRequest(this.error);
+}
+
+class ErrorOperationFailed implements ErrorBase {
+  ErrorOperationFailed();
 }
