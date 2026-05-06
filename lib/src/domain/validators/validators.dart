dca599840476c48e58078b6e77a86e6da7b66057
Wesley Santos <wesleypro37@gmail.com>
2026-05-06 09:52:52 -0300

    feat: add validation classes for email, IP, password, and URL with example usage

---
+++ lib/src/domain/validators/validators.dart
@@ -0,0 +1,7 @@
+export 'email_validator.dart';
+export 'ip_validator.dart';
+export 'password_validator.dart';
+export 'url_validator.dart';
+export 'validation_error.dart';
+export 'validation_result.dart';
+export 'validator.dart';
